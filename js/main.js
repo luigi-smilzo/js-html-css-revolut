4b62de605db2977f278f25a3d056c7bdce46bb32
Luigi <luigismilzo@live.com>
2020-04-14 18:35:12 +0200

    Dropdown "Prodotti" toggle

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready( function(){
+    var dropdown = $('.dropdown a');
+
+    dropdown.click(function() {
+        $(this).next('.dropdown-menu').toggle();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
