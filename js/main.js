d11c9e848b1eddbdea9d0b658dcbc91365ca74e9
Luigi <luigismilzo@live.com>
2020-04-14 19:13:49 +0200

    Language arrow toggle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 $(document).ready( function(){
+    // REFERENCES
     var dropdown = $('.dropdown a');
-    
-
+    var language = $('.language');
+    // EVENTS
     dropdown.click(function() {
         var item = $(this);
         item.next('.dropdown-menu').toggle();
         item.toggleClass('selected');
-        
+    });
+
+    language.mouseenter(function (){
+        $(this).next().toggleClass('fa-angle-down fa-angle-up');
+    });
+    
+    language.mouseleave(function (){
+        $(this).next().toggleClass('fa-angle-down fa-angle-up');
     });
 
 
@@ -15,19 +23,4 @@ $(document).ready( function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+}); /* <- End Ready */
